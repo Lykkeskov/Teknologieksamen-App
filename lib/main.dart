5b8467f8fb39fdb2fae319e8da78f624749eb47b
shitman2 <h.lykkeskov@gmail.com>
2025-04-29 14:29:50 +0200

    Added some stuff. Still not working

--- lib/main.dart
+++ lib/main.dart
@@ -1,122 +1,104 @@
 import 'package:flutter/material.dart';
+import 'bluetooth_manager.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'StepCue',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const BluetoothScannerPage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+class BluetoothScannerPage extends StatefulWidget {
+  const BluetoothScannerPage({super.key});
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<BluetoothScannerPage> createState() => _BluetoothScannerPageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _BluetoothScannerPageState extends State<BluetoothScannerPage> {
+  final BluetoothManager _bluetoothManager = BluetoothManager();
+  bool _isScanning = false;
+  List<BluetoothDevice> _devices = [];
 
-  void _incrementCounter() {
+  @override
+  void initState() {
+    super.initState();
+    _initializeBluetooth();
+  }
+
+  Future<void> _initializeBluetooth() async {
+    await _bluetoothManager.initialize();
+    _bluetoothManager.deviceStream.listen((devices) {
+      setState(() {
+        _devices = devices;
+      });
+    });
+  }
+
+  void _toggleScan() async {
+    if (_isScanning) {
+      await _bluetoothManager.stopScan();
+    } else {
+      await _bluetoothManager.startScan();
+    }
+    
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      _isScanning = !_isScanning;
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: const Text('StepCue'),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Text(
+              _isScanning ? 'Scanning for devices...' : 'Not scanning',
+              style: const TextStyle(fontSize: 18),
             ),
-          ],
-        ),
+          ),
+          Expanded(
+            child: _devices.isEmpty
+                ? const Center(child: Text('No devices found'))
+                : ListView.builder(
+                    itemCount: _devices.length,
+                    itemBuilder: (context, index) {
+                      final device = _devices[index];
+                      return ListTile(
+                        title: Text(device.name.isNotEmpty ? device.name : 'Unknown Device'),
+                        subtitle: Text(device.id),
+                        trailing: const Icon(Icons.bluetooth),
+                        onTap: () {
+                          // Handle device selection
+                        },
+                      );
+                    },
+                  ),
+          ),
+        ],
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+        onPressed: _toggleScan,
+        child: Icon(_isScanning ? Icons.stop : Icons.search),
+      ),
     );
   }
 }
